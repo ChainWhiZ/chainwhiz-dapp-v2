d6a1ba22a363eca0c4f07ea4ccb6f793eb82d834
xander <shuaibualexander@gmail.com>
2022-10-24 23:19:06 +0100

    feature: include community section

---
+++ __pages__/home/segments/community/index.tsx
@@ -0,0 +1,31 @@
+import RenderStyledImage from 'components/image/renderstyledimage';
+import styles from './community.module.scss';
+
+export default function Community() {
+  return (
+    <section className={styles.community__wrapper}>
+      <div className={styles.community}>
+        <p>Join the guild</p>
+        <h3>Your voice. Your opinions. Your Community.</h3>
+        <h5>
+          Community is the heart and soul of Chainwhiz. Join us in our journey.
+        </h5>
+
+        <div className={styles.button_group}>
+          <div>
+            <RenderStyledImage
+              className={styles.social__image}
+              src="/images/landing/community/discord.svg"
+            />
+          </div>
+          <div>
+            <RenderStyledImage
+              className={styles.social__image}
+              src="/images/landing/community/twitter.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
