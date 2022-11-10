bebdb861ac443a0575448546e3648ad777cff089
xander <shuaibualexander@gmail.com>
2022-11-10 01:01:18 +0100

    chore: resolve merge conflicts

--- components/navbar/Dropdown/index.tsx
+++ components/navbar/Dropdown/index.tsx
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import PostBounty from './postbounty';
-import styles from './dropdown.module.scss';
-
-export default function index({ onconfirm }: any) {
-  const [bounties, setBounties] = useState(false);
-  const [postbounty, setPostbounty] = useState(false);
-  const [explorebounty, setExplorebounty] = useState(false);
-
-=======
 import React from 'react';
 import styles from './dropdown.module.scss';
 
 export default function Dropdown() {
->>>>>>> adff9b85fcba94c1be3fe316290b2fae0829a5c2
   return (
     <div className={styles.menu_wrapper}>
       <div className={styles.menu}>
